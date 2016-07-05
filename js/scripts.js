41816faa298e3712817ef6b9e211cb8805ed92a0
Tyler Morren <tmorren1@gmail.com>
2016-07-05 16:03:35 -0400

    Cosmetic Updates
    Update the styling for multiple sections of the site.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
 var app = angular.module('projectsApp', []);
 
 app.controller('projectsCtrl', function($scope){
+
 	$scope.projects = [
 		{
 			"name" : "Portfolio Website", 
 	    	"description" : "This is my Portfolio Website",
 	    	"image" : "project-portfolio.png",
+	    	"link" : "https://www.tylermorren.com",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS"]
 		},
 		{
-			"name" : "Portfolio Website", 
-	    	"description" : "This is my Portfolio Website",
-	    	"image" : "project-portfolio.png",
-	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS"]
+			"name" : "TODO List", 
+	    	"description" : "A TODO List that allows the user to save the data to local storage. ",
+	    	"image" : "project-todo.png",
+	    	"link" : "https://www.tylermorren.com/TODO-List",
+	    	"tech" : ["HTML", "CSS", "JavaScript", "AngularJS"]
+		},
+		{
+			"name" : "VerQu Website", 
+	    	"description" : "A TODO List that allows the user to save the data to local storage. ",
+	    	"image" : "project-verqu.png",
+	    	"link" : "https://www.verqu.com",
+	    	"tech" : ["HTML", "CSS", "JavaScript", "WordPress"]
 		}
 	];
 
 });
+
