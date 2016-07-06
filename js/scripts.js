055654138ba24ef8e4869fefe978eeb9a5704acf
Tyler Morren <tmorren1@gmail.com>
2016-07-06 12:38:07 -0400

    Dynamic Projects
    Updated JavaScript so that the projects will all have the same height
    dynamically

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"image" : "project-portfolio.png",
 	    	"link" : "https://www.tylermorren.com",
 	    	"github" : "https://github.com/tmorren/tmorren.github.io",
-	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS"]
+	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS", "test1", "test2"]
 		},
 		{
 			"name" : "TODO List", 
@@ -21,7 +21,7 @@ app.controller('projectsCtrl', function($scope){
 		},
 		{
 			"name" : "VerQu Website", 
-	    	"description" : "A TODO List that allows the user to save the data to local storage. ",
+	    	"description" : "I worked with a designer to create the company site for VerQu",
 	    	"image" : "project-verqu.png",
 	    	"link" : "https://www.verqu.com",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "WordPress"]
@@ -30,11 +30,24 @@ app.controller('projectsCtrl', function($scope){
 
 });
 
-/*var maxHeight = 0;
+
+window.onload = resizeProjects;
+window.onresize = resizeProjects;
+
+function resizeProjects(){
+	var maxHeight = 0;
+	/*Resets the height to auto so that it 
+	can correctly find the max height*/
 	$(".projectContain").each(function(){
-	   if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+		$(this).height('auto');
+
+	   	if ($(this).height() > maxHeight) { 
+	   		maxHeight = $(this).height(); 
+	   	}
 	});
 
-	$(".projectContain").height(maxHeight);
-
-	console.log(maxHeight);*/
+	/* Set the height of all projects to the max height */
+	$(".projectContain").each(function(){
+	   		$(this).height(maxHeight); 
+	});
+}
