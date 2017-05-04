b86eab28e3ca836f818ccbe99da2b5f28d82f194
Tyler <tmorren1@gmail.com>
2017-05-03 21:59:18 -0400

    Update 5/3/2017
    Added two new projects and a picture or myself

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,25 @@ app.controller('projectsCtrl', function($scope){
 	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS", "Responsive"]
 		},
 		{
+			"name" : "Synkros.io", 
+	    	"description" : "Synkros is an application for timesheets. Each client is dynamically given a subdomain and database.",
+	    	"link" : "http://www.synkros.io",
+	    	"image" : "project-synkros.png",
+	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "PHP", "MySQL"]
+		},
+		{
 			"name" : "TODO List", 
 	    	"description" : "A TODO List that allows the user to save the data to local storage. ",
 	    	"image" : "project-todo.png",
 	    	"link" : "https://www.tylermorren.com/TODO-List",
 	    	"github" : "https://github.com/tmorren/TODO-List",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "AngularJS", "Responsive"]
+		},
+		{
+			"name" : "VerQu File Uploader", 
+	    	"description" : "A simple file upload WordPress Plugin. Allows for restricted file types and max file size.",
+	    	"image" : "project-vfu.png",
+	    	"tech" : ["HTML", "CSS", "PHP", "WordPress"]
 		},
 		{
 			"name" : "VerQu Website", 
