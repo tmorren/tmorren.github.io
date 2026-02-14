cbd0f37051dbbc0456fdc8efb617140401c81ac5
Tyler Morren <tmorren1@gmail.com>
2026-02-13 21:22:21 -0500

    Add company tag to projects

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"description" : "This is my Portfolio Website to showcase the work I have done.",
 	    	"image" : "project-portfolio.png",
 	    	"github" : "https://github.com/tmorren/tmorren.github.io",
+				"company" : "Personal Project",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "AngularJS", "Responsive"]
 		},
 		{
@@ -15,6 +16,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"description" : "Part of team that created the CRM Lightspeed Quantum. Click link for more info.",
 	    	"link" : "https://www.lightspeedvoice.com/products-quantum",
 	    	"image" : "project-quantum.png",
+				"company" : "Lightspeed Voice",
 	    	"tech" : ["Team Management", "Angular2+", "Laravel", "NodeJS", "Socket.io", "MySQL"]
 		},
 		{
@@ -22,6 +24,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"description" : "Co-founder/developer. Platform to play games and video chat with friends.",
 	    	"link" : "https://www.gamepack.io/",
 	    	"image" : "project-gamepack.png",
+	    	"company" : "Visual Zone Development",
 	    	"tech" : ["HTML", "CSS", "Angular2+", "NodeJS", "MongoDB", "Socket.io", "Express", "WebRTC", "Heroku"]
 		},
 		{
@@ -29,12 +32,14 @@ app.controller('projectsCtrl', function($scope){
 	    	"description" : "To read a desciption of the project, click the link below.",
 	    	"link" : "http://www.tylermorren.com/Synkros-Info",
 	    	"image" : "project-synkros.png",
+	    	"company" : "VerQu",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery", "PHP", "MySQL"]
 		},
 		{
 			"name" : "NBA Compare", 
 	    	"description" : "A fantasy NBA comparison utility. Save leagues to local storage.",
 	    	"image" : "project-nbacompare.png",
+	    	"company" : "Personal Project",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "Angular2+", "Angular Material"]
 		},
 		{
@@ -42,6 +47,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"description" : "A modern clone of the NBA website developed using Angular4.",
 	    	"image" : "project-nbastats.png",
 	    	"github" : "https://github.com/tmorren/nba-stats",
+				"company" : "Personal Project",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "Angular2+", "ChartJS"]
 		},
 		{
@@ -50,6 +56,7 @@ app.controller('projectsCtrl', function($scope){
 	    	"image" : "project-colormatch.png",
 	    	"link" : "http://www.tylermorren.com/colorMatch/",
 	    	"github" : "https://github.com/tmorren/colorMatch/tree/gh-pages",
+				"company" : "Personal Project",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "jQuery"]
 		},
 		{
@@ -58,18 +65,21 @@ app.controller('projectsCtrl', function($scope){
 	    	"image" : "project-todo.png",
 	    	"link" : "https://www.tylermorren.com/TODO-List",
 	    	"github" : "https://github.com/tmorren/TODO-List/tree/gh-pages",
+				"company" : "Personal Project",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "AngularJS", "Responsive"]
 		},
 		{
 			"name" : "VerQu File Uploader", 
 	    	"description" : "A simple file upload WordPress Plugin. Allows for restricted file types and max file size.",
 	    	"image" : "project-vfu.png",
+	    	"company" : "VerQu",
 	    	"tech" : ["HTML", "CSS", "PHP", "WordPress"]
 		},
 		{
 			"name" : "VerQu Website", 
 	    	"description" : "I worked with a designer to create the company site for VerQu.",
 	    	"image" : "project-verqu.png",
+				"company" : "VerQu",
 	    	"tech" : ["HTML", "CSS", "JavaScript", "WordPress", "Responsive"]
 		}
 	];
